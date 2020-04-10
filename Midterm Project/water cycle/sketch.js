61911f06dafa25cb35460a9a6ccbafd5cef3bc2d
Zhao Yang <yangzhao7508@outlook.com>
2020-04-11 01:23:09 +0800

    midterm progress

--- Midterm Project/water cycle/sketch.js
+++ Midterm Project/water cycle/sketch.js
@@ -2,43 +2,189 @@ const C_GRAVITY = 0.1;
 let snowflakes = [];
 let particles = [];
 let snows = [];
-let moving = false;
+let rains = [];
+let sinArray = [];
+let sinResolution = 360 * 4;
+let waves = [];
+let max = 10;
+let count = 0;
+let cracked = false;
+
+let wind;
+let rain;
+let generating_ice;
+let cracking_ice;
+let water;
+
 
 // JSON
 let params = {
   temperature: -10,
-  wind: 0,
+  wind: 0.05,
   size: 40,
+  amp: 0.4,
+  freq: 0.017,
   debug_mode: false
 }
 
 const gui = new dat.GUI();
 const f1 = gui.addFolder('General');
 const f2 = gui.addFolder('Snowflake');
+const f3 = gui.addFolder('Wave');
 
 f1.add(params, 'temperature').max(35).min(-20).step(0.1);
-f1.add(params, 'wind').max(0.5).min(-0.5).step(0.005);
+f1.add(params, 'wind').max(0.5).min(-0.5).step(0.05);
 
 f2.add(params, 'size').max(100).min(20).step(5);
 
+f3.add(params, 'amp', 0.1, 0.8);
+f3.add(params, 'freq', 0.015, 0.02);
+
 gui.add(params, 'debug_mode');
+
+function preload() {
+  wind = loadSound("sound/wind.mp3");
+  rain = loadSound("sound/rain.mp3");
+  generating_ice = loadSound("sound/g_ice.mp3");
+  cracking_ice = loadSound("sound/c_ice.mp3");
+  water = loadSound("sound/water.mp3");
+}
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+
+  for (let i=0; i<sinResolution; i++) {
+    let angle = map(i, 0, sinResolution, 0, TWO_PI);
+    sinArray.push( sin(angle) );
+  }
+
+  for (let x = 0; x < width; x++){
+    waves.push(new Wave(x, height - 50, height - 50, params.freq, params.amp));
+  }
+
+  wind.playMode('restart');
+  rain.playMode('restart');
+  generating_ice.playMode('restart');
+  cracking_ice.playMode('restart');
+  water.playMode('restart');
+
 }
 
 function draw() {
   background(0);
 
-  if (params.temperature <= 0){
-    for (let i = 0; i < (-params.temperature) / 5; i ++){
-      snows.push(new Snow(random(width), random(-50, 0), random(2,4)));
+  // play the sound effect of the wind
+  if (params.wind != 0){
+    wind.setVolume(map(abs(params.wind), 0, 0.5, 0.05, 0.8));
+    if(!wind.isPlaying()){
+      wind.play();
+    }
+  }else{
+    if ( wind.isPlaying()){
+      wind.stop();
+    }
+  }
+
+  // play the sound effect of the rain and water
+  if (params.temperature > 0){
+    // water sound
+    if(!water.isPlaying()){
+      water.setVolume(0.2);
+      water.play();
+    }
+    // rain sound
+    rain.setVolume(map(rains.length, 0, 800, 0.05, 0.6));
+    if (!rain.isPlaying()){
+      rain.play();
+    }
+  }else{
+    if (rain.isPlaying()){
+      rain.stop();
+    }
+    if (water.isPlaying()){
+      water.stop();
     }
   }
 
 
+
+  if (params.temperature <= 0){
+    let m = max + params.temperature;
+    if (m < 0){
+      for ( let i = 0; i < (-m) / 3; i ++){
+        snows.push(new Snow(random(width), random(-50, 0), random(2,4)));
+      }
+    }else{
+      if (count == 0){
+        snows.push(new Snow(random(width), random(-50, 0), random(2,4)));
+        count ++;
+      }else{
+        count ++;
+        if (count > m){
+          count = 0;
+        }
+      }
+    }
+  }else{
+    let m = max - params.temperature;
+    if (m < 0){
+      for ( let i = 0; i < (-m) / 8; i ++){
+        rains.push(new Rain(random(width), random(-50, 0), random(5,8)))
+      }
+    }else{
+      if (count == 0){
+        rains.push(new Rain(random(width), random(-50, 0), random(5,8)))
+        count ++;
+      }else{
+        count ++;
+        if (count > m){
+          count = 0;
+        }
+      }
+    }
+  }
+
+  // display waves
+  for (let i = 0; i < waves.length; i ++){
+    let w = waves[i];
+
+    w.update();
+    w.display();
+  }
+
+  // display rains
+  for (let i = 0; i < rains.length; i ++){
+    let r = rains[i];
+
+    // gravity
+    let gravity = createVector(0, C_GRAVITY * r.mass);
+    r.applyForce( gravity );
+
+    // resistance
+    let resistance = p5.Vector.mult(r.vel, -1);
+    resistance.mult(0.1);
+    r.applyForce( resistance );
+
+    // wind force
+    let wind = createVector(params.wind, 0);
+    r.applyForce( wind );
+
+    r.update();
+    r.checkEdges( waves );
+    r.display();
+  }
+
+  // remove the snows which are done
+  for (let i = rains.length - 1; i >= 0; i --){
+    let r = rains[i];
+    if(r.isDone){
+      rains.splice(i, 1);
+    }
+  }
+
+  // display snows
   for (let i = 0; i < snows.length; i ++){
     let s = snows[i];
 
@@ -55,11 +201,8 @@ function draw() {
     let wind = createVector(params.wind, 0);
     s.applyForce( wind );
 
-    // update the lifeReduction based on the terperature
-    //s.lifeReduction += (params.temperature / 10000);
-
     s.update();
-    s.checkEdges();
+    s.checkEdges( waves );
     s.display();
   }
 
@@ -74,14 +217,31 @@ function draw() {
   // update and display snowflakes
   for (let i =0; i < snowflakes.length; i ++){
     let f = snowflakes[i];
-    if (f.updating){
-      f.update();
-    }
-    f.display();
-    if (moving){
+    if (params.temperature <= 0){
+      cracked = false;
+      if (f.updating){
+        f.update();
+      }
+      if (f.moving){
+        f.move();
+        if (f.particles.length == 0){
+          snowflakes.splice(i,1);
+        }
+      }else{
+        f.display();
+      }
+    }else{
+      if (!cracked && !cracking_ice.isPlaying()){
+        cracking_ice.setVolume(0.2);
+        cracking_ice.play();
+      }
+      cracked = true;
+      f.moving = true;
       f.move();
+      if (f.particles.length == 0){
+        snowflakes.splice(i,1);
+      }
     }
-
   }
 
 
@@ -91,24 +251,89 @@ function draw() {
     snows.splice(0, 1);
   }
 
+  // limit the number of the rainss
+  while(rains.length > 800){
+    rains.splice(0, 1);
+  }
+
   // debug mode
   if(params.debug_mode){
     fill(255);
     text("Snow number: " + snows.length, 10, 20);
-    text("FrameRate: " + Math.floor(frameRate()), 10, 40);
+    text("Rain number: " + rains.length, 10, 40);
+    text("FrameRate: " + Math.floor(frameRate()), 10, 60);
   }
 
 }
 
 function mouseClicked(){
   if (params.temperature <= 0){
-    snowflakes.push( new Snowflake(mouseX, mouseY));
+    let generated = true;
+    for (let i = 0; i < snowflakes.length; i ++){
+      let s = snowflakes[i];
+      if(dist(mouseX, mouseY, s.pos.x, s.pos.y) < s.size){
+        s.moving = true;
+        generated = false;
+        cracking_ice.setVolume(0.2);
+        cracking_ice.play();
+      }
+    }
+    if (generated){
+      snowflakes.push( new Snowflake(mouseX, mouseY));
+      generating_ice.setVolume(0.2);
+      generating_ice.play();
+    }
   }
 }
 
-function keyPressed(){
-  if (key == ' '){
-    moving = !moving;
+function mSin( angle ) {
+  // won't work with negative angle values!
+  let index = parseInt( map(angle % TWO_PI, 0, TWO_PI, 0, sinResolution) );
+  return sinArray[index];
+}
+
+class Wave{
+  constructor(x, y, yOffset, adjFreq, adjAmp){
+    this.pos = createVector(x, y);
+    this.adjFreq = adjFreq;
+    this.adjAmp = adjFreq;
+    this.yOffset = yOffset;
+  }
+
+  update(){
+    let freq; // time, angle (+ position)
+    let amp; // radDistance
+
+    this.adjFreq = params.freq;
+    this.adjAmp = params.amp;
+
+    if (params.temperature > 0){
+      freq = this.pos.x * 0.005 + frameCount * 0.01;
+      amp = 20;
+      let sinForAmp = mSin(freq) * amp;
+
+      freq = this.pos.x * this.adjFreq;
+      amp = sinForAmp * this.adjAmp;
+      let sinValue = mSin(freq) * amp;
+
+      this.pos.y = this.yOffset + sinValue;
+    }
+  }
+
+  display(){
+    if (params.temperature > 0){
+      push();
+      stroke(151, 171, 232);
+      strokeWeight(2);
+      line(this.pos.x, height, this.pos.x, this.pos.y);
+      pop();
+    }else{
+      push();
+      stroke(0, 238, 255);
+      strokeWeight(2);
+      line(this.pos.x, height, this.pos.x, this.pos.y);
+      pop();
+    }
   }
 }
 
@@ -118,9 +343,12 @@ function keyPressed(){
 class Snowflake{
   constructor(x, y){
     this.pos = createVector(x, y);
+    this.vel = createVector(0, -0.1);
     this.particles = [];
+    this.size = params.size;
     this.count = 0;
     this.max = params.size / 4;
+    this.moving = false;
     this.updating = true;
   }
 
@@ -153,7 +381,7 @@ class Snowflake{
   }
 
   update(){
-    let current = new Particle(params.size, 0);
+    let current = new Particle(this.size, 0, this.pos.x, this.pos.y);
 
     while (!current.finished && !current.intersects(this.particles)){
       current.update();
@@ -168,35 +396,40 @@ class Snowflake{
   }
 
   move(){
-    for (let i = 0; i < this.particles.length; i ++){
-      let p = this.particles[i];
+    if (this.moving){
+      this.pos.add(this.vel);
 
-      // gravity
-      let gravity = createVector(0, C_GRAVITY * p.mass);
-      p.applyForce( gravity );
+      for (let i = 0; i < this.particles.length; i ++){
+        let p = this.particles[i];
 
-      // resistance
-      let resistance = p5.Vector.mult(p.vel, -1);
-      resistance.mult(0.1);
-      p.applyForce( resistance );
+        // gravity
+        let gravity = createVector(0, C_GRAVITY * p.mass);
+        p.applyForce( gravity );
 
-      // wind force
-      let wind = createVector(params.wind, 0);
-      p.applyForce( wind );
+        // resistance
+        let resistance = p5.Vector.mult(p.vel, -1);
+        resistance.mult(0.1);
+        p.applyForce( resistance );
 
-      push();
-      translate(this.pos.x, this.pos.y);
-      p.updateLifespan();
-      p.move();
-      p.show();
-      pop();
-    }
+        // wind force
+        let wind = createVector(params.wind, 0);
+        p.applyForce( wind );
 
-    // remove the snows which are done
-    for (let i = this.particles.length - 1; i >= 0; i --){
-      let p = this.particles[i];
-      if(p.isDone){
-        this.particles.splice(i, 1);
+        push();
+        translate(this.pos.x, this.pos.y);
+        p.updateLifespan();
+        p.move();
+        p.checkEdges();
+        p.show();
+        pop();
+      }
+
+      // remove the snows which are done
+      for (let i = this.particles.length - 1; i >= 0; i --){
+        let p = this.particles[i];
+        if(p.isDone){
+          this.particles.splice(i, 1);
+        }
       }
     }
   }
@@ -208,6 +441,7 @@ class Particle{
     this.pos = createVector(x, y);
     this.vel = createVector();
     this.acc = createVector();
+    this.centerpos = createVector(cx, cy);
     this.mass = 2;
     this.rad = 2;
     this.tem = params.temperature;
@@ -262,6 +496,19 @@ class Particle{
     this.acc.add( force );
   }
 
+  checkEdges() {
+    if (this.pos.x < 0 - this.centerpos.x) {
+      this.pos.x = 0 - this.centerpos.x;
+      this.vel.x *= -0.5;
+    }else if (this.pos.x > width - this.centerpos.x) {
+      this.pos.x = width - this.centerpos.x;
+      this.vel.x *= -0.5;
+    }else if (this.pos.y > height - 40 - this.centerpos.y) {
+      this.pos.y = height - 40 - this.centerpos.y;
+    }
+
+  }
+
   // check whether the particle intersects other particles
   intersects(array){
     let result = false;
@@ -281,7 +528,7 @@ class Particle{
   show(){
     push();
     translate(this.pos.x, this.pos.y);
-    fill(255);
+    fill(255, 500 * this.lifespan);
     // stroke(255);
     noStroke();
     ellipse(0, 0, this.rad * 2, this.rad * 2);
@@ -332,15 +579,16 @@ class Snow {
 
   }
 
-  checkEdges() {
+  checkEdges( a ) {
     if (this.pos.x < 0) {
       this.pos.x = 0;
       this.vel.x *= -0.5;
     }else if (this.pos.x > width) {
       this.pos.x = width;
       this.vel.x *= -0.5;
-    }else if (this.pos.y > height- 20) {
-      this.pos.y = height - 20;
+    }else if (this.pos.y > a[int(this.pos.x)].pos.y) {
+      this.pos.y = a[int(this.pos.x)].pos.y;
+      this.vel.y *= -0.1;
     }
   }
 
@@ -353,8 +601,102 @@ class Snow {
   display() {
     push();
     translate(this.pos.x, this.pos.y);
-    fill(255, 500 * this.lifespan);
+    fill(255, 1000 * this.lifespan);
+    noStroke();
     ellipse(0, 0, this.rad, this.rad);
+    pop();
+
+    if (params.debug_mode){
+      fill(255);
+      text( int(this.lifeReduction * 1000), this.pos. x+10, this.pos. y);
+    }
+  }
+
+}
+
+class Rain{
+  constructor(x, y, m) {
+    // properties
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector();
+    this.mass = m;
+    this.len = m * 3;
+    this.tem = params.temperature;
+    this.edge = false;
+    this.frozen = false;
+
+    // Lifespan of the snow
+    this.lifespan = 1.0; // 100%
+    this.lifeReduction = random(0.001, 0.004) + this.tem / 10000;
+    this.isDone = false;
+  }
+
+  update() {
+
+    this.vel.add( this.acc );
+    this.pos.add( this.vel );
+    this.acc.mult(0);
+
+    // update lifeReduction
+    if (params.temperature > 0){
+      this.frozen = false;
+      if (this.tem != params.temperature){
+        this.lifeReduction += (params.temperature - this.tem) / 10000;
+        this.tem = params.temperature;
+      }
+    }else{
+      this.frozen = true;
+    }
+
+    // update lifespan
+    this.lifespan -= this.lifeReduction;
+    if (this.lifespan <= 0.0) {
+      this.isDone = true;
+    }
+
+  }
+
+  checkEdges( a ) {
+    if (this.pos.x < 0) {
+      this.pos.x = 0;
+      this.vel.x *= -0.3;
+      this.edge = true;
+    }else if (this.pos.x > width) {
+      this.pos.x = width;
+      this.vel.x *= -0.3;
+      this.edge = true;
+    }else if (this.pos.y > a[int(this.pos.x)].pos.y) {
+      this.pos.y = a[int(this.pos.x)].pos.y;
+      this.vel.y *= -0.1;
+      this.edge = true;
+    }
+
+  }
+
+  applyForce( f ) {
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add( force );
+  }
+
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    if (this.edge){
+      if (this.frozen){
+        fill(255, 500 * this.lifespan);
+      }else{
+        fill(135,105,160, 500 * this.lifespan);
+      }
+      noStroke();
+      ellipse(0, 0, this.len * this.lifespan, this.len * this.lifespan);
+    }else{
+      rotate(-params.wind);
+      stroke(135,105,160, 500 * this.lifespan);
+      line(0, -this.len, 0, 0);
+    }
+
     pop();
 
     if (params.debug_mode){
@@ -367,7 +709,7 @@ class Snow {
 
 // the way to display a snowflake
 // push();
-// translate(mouseX, mouseY);
+// translate(width/2, height/2);
 // rotate(PI/6);
 //
 // let current = new Particle(30, 0);
@@ -395,3 +737,30 @@ class Snow {
 // }
 // pop();
 //
+
+// wave function
+// function wave(yOffset, adjFreq, adjAmp, r, g, b) {
+//   let freq; // time, angle (+ position)
+//   let amp; // radDistance
+//
+//   for (let x = 0; x < width; x++) {
+//     let y = yOffset;
+//     if (params.temperature > 0){
+//       freq = x * 0.005 + frameCount * 0.01;
+//       amp = 20;
+//       sinForAmp = mSin(freq) * amp;
+//
+//       freq = x * adjFreq;
+//       amp = sinForAmp * adjAmp;
+//       let sinValue = mSin(freq) * amp;
+//
+//       y += sinValue;
+//     }
+//
+//     push();
+//     stroke(r, g, b);
+//     strokeWeight(2);
+//     line(x, height,x,y);
+//     pop();
+//   }
+// }
---
+++ NOC sample code/Midterm-Project(1)/Midterm-Project/js/sketch.js
@@ -0,0 +1,543 @@
+const C_GRAVITY = 4;
+let electrons = [];
+let protrons = [];
+let neutrons = [];
+let particles1 = [];
+let particles2 = [];
+let core;
+// let fade1, fade2;
+let stage = 0;
+let isDone = false;
+let debugMode = false;
+
+
+// JSON
+let params = {
+  attraction: 0.09,
+  adj:10,
+  debugMode:false,
+  angle:1,
+  range:0.2
+
+}
+const gui = new dat.GUI();
+gui.add(params, 'attraction',0.09,0.4);
+gui.add(params, 'adj',10,20);
+gui.add(params, 'debugMode');
+gui.add(params, 'angle',1,3);
+gui.add(params, 'range',0.2,0.5);
+
+function preload(){
+  soundFormats('mp3', 'ogg');
+  song1 = loadSound('assets/fade1.mp3');
+  song2 = loadSound('assets/fade2.mp3');
+  song1.setVolume(0.1);
+  song2.setVolume(0.1);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  // fade1 = loadSong("fade1.mp3");
+  // fade2 = loadSong("fade2.mp3");
+
+  for (let i=0; i<80; i++) {
+    particles1.push( new Particle(random(width), random(height), random(1,2)) ); // play with the mass value!
+  }
+
+  for (let i=0; i<600; i++) {
+    particles2.push( new Particle(random(width), random(height), random(1,2)) ); // play with the mass value!
+  }
+}
+
+function draw() {
+  background(0,20)
+
+  if(params.debugMode){
+    fill(255);
+    textSize(20);
+    text(nf(frameRate(),2,0),100,100);
+  }
+
+  if(isDone==false){
+    push();
+
+    let freq = frameCount * 0.05;
+    let amp = 30;
+    let sinValue = sin(freq) * amp;
+    let r =70
+
+    noFill();
+    stroke(255);
+    strokeWeight(4);
+    ellipse(width/2,height/2,r+sinValue,r+sinValue);
+    pop();
+
+    if((mouseX>width/2-50&&mouseX<width/2+50)&&(mouseY>height/2-50&&mouseY<height/2+50)&&mousePressed){
+      isDone = true;
+    }
+  }
+
+  if(stage<6){
+    song1.play();
+  }
+
+  if(isDone==true){
+    core = new Particle(width/2,height/2,400);
+    let centerPos = createVector(width/2,height/2)
+
+    // ADD electrons
+    for(let a=0; a<electrons.length; a++){
+      let e = electrons[a];
+
+      e.attractTo( core.pos );
+      e.update();
+      e.display();
+
+    }
+
+    // ADD Neutron
+    for(let i = 0; i< 10; i++){
+      neutrons.push(new Neutron(random(width/2-30,width/2+30),random(height/2-10,height/2+10),random(5,10)));
+    }
+
+    for(let i=0; i < 10; i++){
+      let n = neutrons[i];
+
+      n.vel = createVector(random(-1,1),random(-1,1));
+      n.applySimpleAttraction(core);
+      n.update();
+      n.display();
+    }
+
+    // ADD protrons
+    for(let i = 0; i< 10; i++){
+      protrons.push(new Protron(random(width/2-20,width/2+20),random(height/2-20,height/2+20),random(20,30)));
+    }
+
+    for(let i=0; i < 10; i++){
+      let p = protrons[i];
+
+      p.vel = createVector(random(-1,1),random(-1,1));
+      p.applySimpleAttraction(core);
+      p.update();
+      p.display();
+    }
+
+    //First layer particle sphere
+    if (stage>6) {
+      song2.play();
+      for(let a=0; a<particles1.length; a++) {
+        let p1 = particles1[a];
+
+        p1.applySimpleAttraction( core, 7 );
+        p1.update();
+        p1.display();
+      }
+
+      //Second layer particle sphere
+
+      for(let a=0; a<particles2.length; a++) {
+        let p2 = particles2[a];
+
+        p2.applySimpleAttraction( core );
+        p2.update();
+        p2.display();
+      }
+    }
+    // for(let a=0; a<particles1.length; a++) {
+    //   let p1 = particles1[a];
+    //
+    //   p1.applySimpleAttraction( core, 7 );
+    //   p1.update();
+    //   p1.display();
+    // }
+    //
+    // //Second layer particle sphere
+    //
+    // for(let a=0; a<particles2.length; a++) {
+    //   let p2 = particles2[a];
+    //
+    //   p2.applySimpleAttraction( core );
+    //   p2.update();
+    //   p2.display();
+    // }
+    //
+    //
+    // // ADD electrons
+    // for(let a=0; a<electrons.length; a++){
+    //   let e = electrons[a];
+    //
+    //   e.attractTo( core.pos );
+    //   e.update();
+    //   e.display();
+    //
+    // }
+
+  }
+}
+
+function mousePressed(){
+  stage += 1;
+  let e = new Electron(mouseX,mouseY,random(5,10));
+  //e.vel = createVector(random(-2,2),random(-2,2));
+
+  let vector = p5.Vector.sub(core.pos, e.pos);
+  // vector.rotate(PI/2); // 90 degree angle
+  if (random(1) < 0.5) {
+    if(random(1)< 0.5){
+      vector.rotate(PI/2);
+    }else {
+      vector.rotate(-PI/2);
+    }
+    vector.mult( params.angle );
+  } else {
+    if(random(1)< 0.5){
+      vector.rotate(PI/2);
+    }else {
+      vector.rotate(-PI/2);
+    }
+    vector.mult( params.angle );
+  }
+  e.vel = vector.copy();
+
+  electrons.push(e);
+}
+
+
+class Neutron {
+  constructor(x,y,m){
+    this.pos = createVector(x,y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = m;
+  }
+  update(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  applyForce(f){
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+
+  applySimpleAttraction(other) {
+    let f = p5.Vector.sub(other.pos, this.pos);
+    let distance = f.mag();
+    f.normalize();
+    f.mult(0.9); // play with this value!
+
+    // *** check this out! ***
+    // if (distance < other.rad) {
+    //   let adj = 30.0; // play with this value!
+    //   f.mult(-1 * adj);
+    // }
+
+    this.applyForce( f );
+  }
+
+  // applyGravitationalAttraction(other) {
+  //   let f = p5.Vector.sub(other.pos, this.pos);
+  //   let distance = f.mag(); // = this.pos.distance( other.pos );
+  //   let gMag = (C_GRAVITY * this.mass * other.mass) / (distance * distance); // mag
+  //   f.normalize(); // direction
+  //   f.mult(gMag);
+  //
+  //   // *** check this out! ***
+  //   if (distance < other.rad) {
+  //     let adj = 10.0; // play with this value!
+  //     f.mult(-1 * adj);
+  //   }
+  //
+  //   this.applyForce( f );
+  // }
+
+  display(){
+    push();
+    let amp, freq;
+    let strokeOpacity, centerOpacity;
+
+    strokeOpacity = 100;
+    centerOpacity = 20;
+
+    amp = random(8,12);
+    freq = frameCount * 0.08;
+    let cosValue = cos(freq) * amp;
+    // translate(this.pos.x, this.pos.y);
+    // fill(100,220,100);
+    // ellipse(0,0,10,10);
+    let opa1 = 255;
+    for(let r = 0; r<25; r+=1){
+      let colorR = map(r,0,20,255,100);
+      let colorG = map(r,0,20,255,180);
+      let colorB = map(r,0,20,255,100);
+      noFill();
+      strokeWeight(1);
+      stroke(colorR,colorG,colorB,opa1)
+      ellipse(this.pos.x, this.pos.y, r+cosValue,r+cosValue);
+      opa1 -= 9;
+    }
+    pop();
+  }
+}
+
+class Protron {
+  constructor(x,y,m){
+    this.pos = createVector(x,y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = m;
+    this.rad = m;
+  }
+  update(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  applySimpleAttraction(other) {
+    let f = p5.Vector.sub(other.pos, this.pos);
+    let distance = f.mag();
+    f.normalize();
+    f.mult(0.9); // play with this value!
+
+    // *** check this out! ***
+    // if (distance > other.rad) {
+    //   let adj = 30.0; // play with this value!
+    //   f.mult(-1 * adj);
+    // }
+
+    this.applyForce( f );
+  }
+
+  // applyGravitationalAttraction(other) {
+  //   let f = p5.Vector.sub(other.pos, this.pos);
+  //   let distance = f.mag(); // = this.pos.distance( other.pos );
+  //   let gMag = (C_GRAVITY * this.mass * other.mass) / (distance * distance); // mag
+  //   f.normalize(); // direction
+  //   f.mult(gMag);
+  //
+  //   // *** check this out! ***
+  //   if (distance > other.rad) {
+  //     let adj = 10.0; // play with this value!
+  //     f.mult(-1 * adj);
+  //   }
+  //
+  //   this.applyForce( f );
+  // }
+
+  applyForce(f){
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+
+  applyRepulsion(other) {
+    let f = p5.Vector.sub(other.pos, this.pos);
+    let distance = f.mag();
+    let gMag = (C_GRAVITY * this.mass * other.mass) / (distance * distance*distance);
+    f.normalize();
+    f.mult(-gMag);
+    this.applyForce( f );
+  }
+
+
+
+  display(){
+    let amp, freq;
+    let strokeOpacity, centerOpacity;
+
+    strokeOpacity = 100;
+    centerOpacity = 20;
+
+    amp = random(5,10);
+    freq = frameCount * 0.05;
+    let sinValue = sin(freq) * amp;
+
+    push();
+    // translate(this.pos.x, this.pos.y);
+    // strokeWeight(1);
+    // stroke(140,100,100,strokeOpacity);
+    // fill(240,100,100,centerOpacity);
+    // ellipse(0,0,this.rad+sinValue,this.rad+sinValue);
+    let opa1 = 255;
+    for(let r = 0; r<25; r+=1){
+      let colorR = map(r,0,20,255,102);
+      let colorG = map(r,0,20,255,186);
+      let colorB = map(r,0,20,255,183);
+      noFill();
+      strokeWeight(1);
+      stroke(colorR,colorG,colorB,opa1)
+      ellipse(this.pos.x, this.pos.y, r+sinValue,r+sinValue);
+      opa1 -= 9;
+    }
+
+    pop();
+  }
+}
+
+
+
+class Electron{
+  constructor(x,y,m){
+    this.pos = createVector(x,y)
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = m;
+    this.rad = m;
+    this.r = 255;
+    this.g = 255;
+    this.b = 255;
+  }
+
+  update(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    //
+
+    this.vel.mult(1);
+    this.vel.limit(25);
+  }
+
+  attractTo(target) {
+  let f = p5.Vector.sub(target, this.pos);
+  //f.normalize(); // get the direction
+  //f.mult(1); // mag is now 5px.
+  f.mult(params.attraction);
+  this.applyForce( f );
+}
+
+// Simple Attraction testing: fail
+
+// applySimpleAttraction(other) {
+//   let f = p5.Vector.sub(other.pos, this.pos);
+//   let distance = f.mag();
+//   f.normalize();
+//   f.mult(0.2); // play with this value!
+//
+//   // *** check this out! ***
+//   if (distance < other.rad) {
+//     // let adj = 30.0; // play with this value!
+//     f.mult(-1 * params.eAdj);
+//   }
+//
+//   this.applyForce( f );
+// }
+
+// GravitationalAttraction testing: fail
+
+// applyGravitationalAttraction(other) {
+//   let f = p5.Vector.sub(other.pos, this.pos);
+//   let distance = f.mag(); // = this.pos.distance( other.pos );
+//   let gMag = (params.k_value * this.mass * other.mass) / (distance * distance); // mag
+//   f.normalize(); // direction
+//   f.mult(gMag);
+//
+//   // *** check this out! ***
+//   if (distance < other.rad/5) {
+//     // let adj = 10.0; // play with this value!
+//     f.mult(-1 * params.eAdj);
+//   }
+//
+//   this.applyForce( f );
+// }
+
+
+
+  applyForce(f){
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+
+  display(){
+    push();
+    let opa1 = 255;
+    for(let r = 0; r<7; r+=1){
+      let colorR = map(r,0,20,230,180);
+      let colorG = map(r,0,20,150,100);
+      let colorB = map(r,0,20,150,100);
+      noFill();
+      strokeWeight(1);
+      stroke(colorR,colorG,colorB,opa1)
+      ellipse(this.pos.x, this.pos.y, r,r);
+      opa1 -= 9;
+    }
+
+    pop();
+  }
+
+}
+
+
+
+class Particle {
+  constructor(x, y, m) {
+    this.pos = createVector(x, y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = m;
+    this.rad = m;
+  }
+
+  applySimpleAttraction(other, adj) {
+    if (adj == undefined || adj < 1) {
+      adj = 1;
+    }
+    let f = p5.Vector.sub(other.pos, this.pos);
+    let distance = f.mag();
+    f.normalize();
+    f.mult(0.5); // play with this value!
+
+    // *** check this out! ***
+
+    if (distance < other.rad/adj) {
+
+      let max = other.rad/adj;
+      let range = params.range; //0.3 0.4 0.5
+      let mag = map(distance, max, max * 0.6, -range/2, range);
+      let value = random(mag - range, mag + range);
+
+      //let range = 5;
+      //let mag = map(distance, max, max * 0.6, -range/2, range);
+      //let freqX = frameCount * 0.005 + this.pos.x * 0.01;
+      //let freqY = frameCount * 0.005 + this.pos.y * 0.01;
+      //let value = noise(freqX, freqY) * mag + random(-mag, mag)*0.5;
+
+      f.mult(-1 * value * params.adj);
+    }
+
+
+    this.applyForce( f );
+  }
+
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+
+    //
+    this.vel.mult(0.97);
+  }
+  applyForce(f) {
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add( force );
+  }
+
+  display() {
+    let opacity = map(cos(frameCount*0.05),-1,1,30,100);
+
+    push();
+    translate(this.pos.x, this.pos.y);
+    noStroke();
+    fill(180,opacity);
+    ellipse(0, 0, this.rad*2, this.rad*2);
+    pop();
+  }
+}
